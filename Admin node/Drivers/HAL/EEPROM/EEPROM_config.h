0b7f13d79bebb8e8c3c86fb0cf8527d309478d43
Mohamed <89197318+MW7000@users.noreply.github.com>
2023-11-10 03:30:29 +0200

    Add files via upload

---
+++ Admin node/Drivers/HAL/EEPROM/EEPROM_config.h
@@ -0,0 +1,13 @@
+/*****************************************
+ * Filename:EEPROM_config.h              *
+ * Description:EEPROM configuration file *
+ * Created on: Nov 2, 2023               *
+ * Author: Zeyad Ossama                  *
+ *****************************************/
+
+#ifndef HAL_EEPROM_EEPROM_CONFIG_H_
+#define HAL_EEPROM_EEPROM_CONFIG_H_
+#define PAGE_SIZE     8
+
+
+#endif /* HAL_EEPROM_EEPROM_CONFIG_H_ */
